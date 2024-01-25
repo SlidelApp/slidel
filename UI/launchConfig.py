1425de97da56bc5b8ae420b678d040cb71503f73
DominicWir <dominicwirasinha@gmail.com>
2024-01-25 19:55:13 +0530

    Minor additions to UI

--- UI/launchConfig.py
+++ UI/launchConfig.py
@@ -1,5 +1,5 @@
 import sys
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QFrame
 from PySide6.QtGui import QGuiApplication
 
 class MyWidget(QWidget):
@@ -15,9 +15,23 @@ class MyWidget(QWidget):
         # Set the window size to the screen size
         self.setGeometry(screen_geometry)
 
+        # Create a QFrame with a blue background
+        frame = QFrame(self)
+        frame.setStyleSheet("background-color: #2C3E50;")
+
+        # Add a yellow border to the QFrame
+        border_style = "2px solid #FBC400;"
+        frame.setStyleSheet(f"{frame.styleSheet()} border: {border_style}")
+
+        # Create a QVBoxLayout and set it for the QWidget
+        layout = QVBoxLayout(self)
+        layout.addWidget(frame)
+
+        # Show the widget in full screen
+        self.showMaximized()
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = MyWidget()
-    widget.show()
     sys.exit(app.exec())
-
--- UI/mainUiOne.py
+++ UI/mainUiOne.py
@@ -2,11 +2,6 @@ from PySide6.QtWidgets import QApplication, QWidget
 from launchConfig import MyWidget
 import sys
 
-# app = QApplication(sys.argv)
-# window = QWidget()
-# window.show()
-# app.exec()
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = MyWidget()
