5b3e28ed4e3134e5dcde0cb439aca15f55db8bc7
Paveisha <95738686+Paveisha@users.noreply.github.com>
2024-02-27 17:21:16 +0530

    Implemented a CustomUser model and added necessary configuration

--- djangoapp/myapp/accounts/models.py
+++ djangoapp/myapp/accounts/models.py
@@ -1,5 +1,28 @@
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
+
+# class CustomUser(AbstractUser):
+#     # Add any additional fields you need for your user model here
+#     pass
+
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 class CustomUser(AbstractUser):
-    pass
+    # Add any additional fields you need for your user model here
+
+    # Add related_name to avoid clashes
+    groups = models.ManyToManyField(
+        Group,
+        verbose_name='groups',
+        blank=True,
+        related_name='customuser_set',
+        related_query_name='user',
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name='user permissions',
+        blank=True,
+        related_name='customuser_set',
+        related_query_name='user',
+    )
