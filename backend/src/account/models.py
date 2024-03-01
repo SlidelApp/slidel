d6f5a3d94fbee695f46bd63f3f4830e8feeb8b56
Paveisha <95738686+Paveisha@users.noreply.github.com>
2024-03-01 18:50:08 +0530

    change: formating

--- backend/src/account/models.py
+++ backend/src/account/models.py
@@ -1,18 +1,19 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db import models
+
 
 class CustomUser(AbstractUser):
     groups = models.ManyToManyField(
         Group,
-        verbose_name='groups',
+        verbose_name="groups",
         blank=True,
-        related_name='customuser_set',
-        related_query_name='user',
+        related_name="customuser_set",
+        related_query_name="user",
     )
     user_permissions = models.ManyToManyField(
         Permission,
-        verbose_name='user permissions',
+        verbose_name="user permissions",
         blank=True,
-        related_name='customuser_set',
-        related_query_name='user',
+        related_name="customuser_set",
+        related_query_name="user",
     )
--- backend/src/account/urls.py
+++ backend/src/account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+
 from .views import create_account
 
 urlpatterns = [
-    path('create/', create_account, name='create_account'),
+    path("create/", create_account, name="create_account"),
 ]
