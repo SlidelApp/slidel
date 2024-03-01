22c6b0d7f864e878693cb9feeb001184a4504953
Paveisha <95738686+Paveisha@users.noreply.github.com>
2024-03-01 10:48:11 +0530

    Copied over changed from the djangoapp

---
+++ backend/src/account/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import create_account
+
+urlpatterns = [
+    path('create/', create_account, name='create_account'),
+]
--- backend/src/account/views.py
+++ backend/src/account/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render  # noqa
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-# Create your views here.
+def create_account(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home') 
+    else:
+        form = UserCreationForm()
+
+        return render(request, 'create_account.html', {'form': form})
+
+
+
