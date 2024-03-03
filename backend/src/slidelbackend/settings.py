8e39d9a227349716bcaade817d6c510e7423e454
Paveisha <95738686+Paveisha@users.noreply.github.com>
2024-03-03 12:51:34 +0530

    add: simple jwt configuration

--- backend/src/slidelbackend/settings.py
+++ backend/src/slidelbackend/settings.py
@@ -131,3 +131,10 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+    'SLIDING_TOKEN_LIFETIME': timedelta(days=14),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=30),
+}
