a5fb424533ab10a344c5f18fe731ae4a40d3db40
Paveisha <95738686+Paveisha@users.noreply.github.com>
2024-03-03 12:33:11 +0530

    Revert "add: add jwt for authentication in views"
    This reverts commit bb9b7ae5f414869cc989df04a511010e8e6ef49f.

--- backend/src/account/views.py
+++ backend/src/account/views.py
@@ -1,12 +1,6 @@
-from multiprocessing import AuthenticationError
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
-from rest_framework.decorators import authentication_classes, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
 
 
 def create_account(request):
@@ -20,11 +14,3 @@ def create_account(request):
         form = UserCreationForm()
 
         return render(request, "create_account.html", {"form": form})
-
-
-@authentication_classes([AuthenticationError])
-@permission_classes([IsAuthenticated])
-class YourApiView(APIView):
-    def get(self, request):
-        # Your view logic here
-        return Response({"message": "Authenticated successfully!"})
