e4c883c6bf9e65c3f4d5746aa3b0ab7fd25d1296
Paveisha <95738686+Paveisha@users.noreply.github.com>
2024-03-03 12:52:53 +0530

    add: url patterns

--- backend/src/account/urls.py
+++ backend/src/account/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
 from .views import create_account
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("create/", create_account, name="create_account"),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
