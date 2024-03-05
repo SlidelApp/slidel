9e5be16d6649450a0aa27b55452bca39d8b828a2
Nazra <ramees.20212089@iit.ac.lk>
2024-03-05 09:14:03 +0530

    new

--- backend/src/account/models.py
+++ backend/src/account/models.py
@@ -1,19 +1,3 @@
-from django.db import models  # noqa
-from django.contrib.auth.models import User
-from django.db import models
-
-class UserSettings(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    username = models.CharField(max_length=150)  # Assuming the same max length as User.username
-    password = models.CharField(max_length=128)  # Assuming the same max length as User.password
-    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)  # Example using ImageField
-
-    def __str__(self):
-        return f"Settings for {self.user.username}"
-
-
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
