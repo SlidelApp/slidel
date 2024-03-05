5dc149c81eeb9f7f1246b76e438fcfac6b1bae85
Nazra <ramees.20212089@iit.ac.lk>
2024-03-05 12:25:06 +0530

    Merge branch 'Setting-Model'

--- backend/src/account/models.py
+++ backend/src/account/models.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import User
 from django.db import models
+
+class UserSettings(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name= models.CharField(max_length=30)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)  # Example using ImageField
+
+    def __str__(self):
+        return f"Settings for {self.user.username}"
 
 
 class CustomUser(AbstractUser):
