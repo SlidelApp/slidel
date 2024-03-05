8459dffa34f0e229159130d128d689912e402cab
Nazra <ramees.20212089@iit.ac.lk>
2024-03-05 13:15:46 +0530

    new commit for setting branch

--- backend/src/account/admin.py
+++ backend/src/account/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin  # noqa
 
+from .models import UserSettings
+
 # Register your models here.
 
-from django.contrib import admin
-from .models import UserSettings
 
 # Register your models here.
 admin.site.register(UserSettings)
--- backend/src/account/models.py
+++ backend/src/account/models.py
@@ -1,14 +1,26 @@
-from django.contrib.auth.models import AbstractUser, Group, Permission
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, Group, Permission, User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class UserSettings(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    display_name= models.CharField(max_length=30)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)  # Example using ImageField
+    display_name = models.CharField(max_length=60)
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures/", default="default_profile_picture.jpg"
+    )
 
     def __str__(self):
         return f"Settings for {self.user.username}"
+
+
+@receiver(post_save, sender=User)
+def create_user_settings(sender, instance, created, **kwargs):
+    if created:
+        # Create a new UserSettings object for the newly created User
+        display_name = instance.last_name + " " + instance.first_name
+        UserSettings.objects.create(user=instance, display_name=display_name)
 
 
 class CustomUser(AbstractUser):
