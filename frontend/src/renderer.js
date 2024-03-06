b09c7c6bc0d80f559ca250832e45a46a76aeef94
Paveisha <95738686+Paveisha@users.noreply.github.com>
2024-03-06 13:16:25 +0530

    add: add 'editPassword' button element

--- frontend/src/renderer.js
+++ frontend/src/renderer.js
@@ -31,6 +31,6 @@ import './index.css';
 document.addEventListener('DOMContentLoaded', () => {
   const editDisplayNameButton = document.getElementById('editDisplayName');
   const editUsernameButton = document.getElementById('editUsername');
-
+  const editPasswordButton = document.getElementById('editPassword');
 });
 
