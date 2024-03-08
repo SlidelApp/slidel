c51d071f56d1adaea121577841c6f7d0f0c5075d
Jason Mishike <jasonmishi@gmail.com>
2024-03-08 13:34:22 +0530

    add signals.py to it's own file

---
+++ backend/src/account/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from account.models import CustomUser, UserSettings
+
+@receiver(post_save, sender=CustomUser)
+def create_user_settings(sender, instance, created, **kwargs):
+    if created:
+        # Create a new UserSettings object for the newly created User
+        display_name = instance.last_name + " " + instance.first_name
+        UserSettings.objects.create(user=instance, display_name=display_name)
+
