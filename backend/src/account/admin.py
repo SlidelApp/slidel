3f39612604865bed0f7ffb6de3df3335fc4a45d2
Jason Mishike <jasonmishi@gmail.com>
2024-03-08 13:42:38 +0530

    fix backend CustomUser not use auth model

--- backend/src/account/admin.py
+++ backend/src/account/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin  # noqa
+from django.contrib.auth.admin import UserAdmin
 
-from .models import UserSettings
-
-# Register your models here.
-
+from account.models import UserSettings, CustomUser
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'is_staff',]
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
 admin.site.register(UserSettings)
