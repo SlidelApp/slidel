35e8b8b4888c8a1243473bc975e7b0b605c1a0d7
dhanujanarada <75336972+dhanujanarada@users.noreply.github.com>
2024-03-20 22:14:27 +0530

    register admins

--- backend/src/slides/admin.py
+++ backend/src/slides/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Slides
 # Register your models here.
+class SlidesAdmin(admin.ModelAdmin):
+    list_display = ["title", "image", "user"]
+    search_fields = ["title"]
+    list_filter = ["user"]
+    list_per_page = 20
+
+admin.site.register(Slides, SlidesAdmin)
+admin.site.register(Slides)
