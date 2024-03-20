f47133f4582ffcf9902d55e5d5c16abf4b3f3856
dhanujanarada <75336972+dhanujanarada@users.noreply.github.com>
2024-03-20 22:14:02 +0530

    add slide model

--- backend/src/slides/models.py
+++ backend/src/slides/models.py
@@ -1,4 +1,17 @@
+
 from django.db import models
 
 # Create your models here.
+class Slides(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="slides_images/")
+    user = models.ForeignKey("account.CustomUser", on_delete=models.CASCADE)
+    
 
+    def __str__(self):
+        return self.title
+    
+
+
+
+        
