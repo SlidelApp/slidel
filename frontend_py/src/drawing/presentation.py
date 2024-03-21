4111d722b400df8de1dc7a50b957a6c30c724d84
dhanujanarada <75336972+dhanujanarada@users.noreply.github.com>
2024-03-22 02:30:20 +0530

    import presentation images

--- frontend_py/src/drawing/presentation.py
+++ frontend_py/src/drawing/presentation.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import os
 
 width, height = 1280, 720
 
@@ -8,9 +9,13 @@ cap = cv.VideoCapture(0)
 cap.set(3, width)
 cap.set(4, height)
 
+# Get the list of presentation images
+folderPath = "Presentation"
+pathImg = sorted(os.listdir(folderPath), key=len)   
+print(pathImg)
+
 while True:
     success, img = cap.read()
     cv.imshow("Image", img)
     if cv.waitKey(1) & 0xFF == ord("q"):
         break
-
