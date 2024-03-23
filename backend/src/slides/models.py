f3dd7fa2b428de4cd4c6f0246c543b55e6182ba0
Jason Mishike <jasonmishi@gmail.com>
2024-03-23 20:35:11 +0530

    fix slides model to be able to upload a file

--- backend/src/slides/models.py
+++ backend/src/slides/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Slides(models.Model):
     title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to="slides_images/")
+    uploaded_file = models.FileField(upload_to="slides/")
     user = models.ForeignKey("account.CustomUser", on_delete=models.CASCADE)
 
     def __str__(self):
