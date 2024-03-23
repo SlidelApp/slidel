001a7168ca06447c239fd17b3138bf9b616d681a
Jason Mishike <jasonmishi@gmail.com>
2024-03-23 20:24:21 +0530

    fix admin not registering slides

--- backend/src/slides/admin.py
+++ backend/src/slides/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
-from .models import Slides
-# Register your models here.
-class SlidesAdmin(admin.ModelAdmin):
-    list_display = ["title", "image", "user"]
-    search_fields = ["title"]
-    list_filter = ["user"]
-    list_per_page = 20
 
-admin.site.register(Slides, SlidesAdmin)
+from .models import Slides
+
 admin.site.register(Slides)
