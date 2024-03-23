1b66c84029bf502b59984bc67a36931a517f2bec
Nazra <ramees.20212089@iit.ac.lk>
2024-03-23 20:26:21 +0530

    Hand Gesture commit

--- backend/src/account/tests.py
+++ backend/src/account/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase  # noqa
+import pytest
+from django.contrib.auth.models import User
+from myapp.models import UserSettings, CustomUser
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_user_settings_creation():
+    user = User.objects.create(username="testuser")
+    assert UserSettings.objects.filter(user=user).exists()
+
+@pytest.mark.django_db
+def test_custom_user_creation():
+    user = CustomUser.objects.create(username="customuser")
+    assert user.groups.count() == 0
+    assert user.user_permissions.count() == 0
+
+@pytest.mark.django_db
+def test_user_settings_str_method():
+    user = User.objects.create(username="testuser")
+    user_settings = UserSettings.objects.create(user=user, display_name="Test User")
+    assert str(user_settings) == "Settings for testuser"
+
