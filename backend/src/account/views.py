b7ab3b4f2ec208a009a1a236070efa7bef504ba5
Jason Mishike <jasonmishi@gmail.com>
2024-03-25 08:03:41 +0530

    fix create use form

--- backend/src/account/views.py
+++ backend/src/account/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -7,16 +6,18 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from account.forms import CustomUserCreationForm
+
 
 def create_account(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect("home")
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
 
         return render(request, "create_account.html", {"form": form})
 
